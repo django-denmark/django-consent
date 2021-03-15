fb172c2619686442bf65aea21cbf4ef16c481543
Benjamin Bach <benjamin@overtag.dk>
2021-03-15 23:33:30 +0100

    Adds a simple configuration of django-ratelimit

--- src/django_consent/settings.py
+++ src/django_consent/settings.py
@@ -8,3 +8,6 @@ UNSUBSCRIBE_SALT = getattr(
 
 #: You can harden security by adding a different salt in your project's settings
 CONFIRM_SALT = getattr(settings, "CONSENT_CONFIRM_SALT", "django-consent-confirm")
+
+#: For more information, `django-ratelimit <https://django-ratelimit.readthedocs.io/en/stable/>`__
+RATELIMIT = getattr(settings, "CONSENT_RATELIMIT", "100/h")
